878d6d1a687cafcb9a3c830ccdf2e10fc453cc8c
yunli <12410922@mail.sustech.edu.cn>
2025-11-13 11:34:19 +0800

    update table creation and visualization on task2

--- SQL/create_table_ddl_statements.sql
+++ SQL/create_table_ddl_statements.sql
@@ -1,105 +1,112 @@
 -- 建表DDL statement
-
+CREATE TYPE gender_enum AS ENUM ('Male','Female');
 -- users
-create type gender_enum as enum ('Male','Female');
-create table users (
-    author_id int primary key ,
-    author_name text not null ,
-    gender gender_enum,
-    age int
+CREATE TABLE users (
+                       author_id        INT PRIMARY KEY,
+                       author_name      TEXT NOT NULL,
+                       gender           gender_enum,
+                       age              INT,
+                       followers_count  INT DEFAULT 0,
+                       following_count  INT DEFAULT 0
 );
 
--- follow关系，成对出现的形式
-create table following (
-    follower_id int not null references users(author_id),
-    followee_id int not null references users(author_id),
-    primary key (follower_id, followee_id)
+-- following 成对属性
+CREATE TABLE following (
+                           follower_id INT NOT NULL REFERENCES users(author_id),
+                           followee_id INT NOT NULL REFERENCES users(author_id),
+                           PRIMARY KEY (follower_id, followee_id)
 );
 
 -- recipes
-create table recipes(
-    recipe_id int primary key ,
-    author_id int not null references users(author_id),
-    recipe_name text not null,
-    description text,
-    recipe_instructions text,
-    recipe_category text,
-    recipe_yield text,
-    recipe_servings int,
-    aggregated_rating numeric(3,2),
-    review_count int
-);
-create table recipe_favorite (
-    user_id   int not null references users(author_id),
-    recipe_id int not null references recipes(recipe_id),
-    primary key (user_id, recipe_id)
+CREATE TABLE recipes(
+                        recipe_id           INT PRIMARY KEY,
+                        author_id           INT NOT NULL REFERENCES users(author_id),
+                        recipe_name         TEXT NOT NULL,
+                        description         TEXT,
+                        recipe_category     TEXT,
+                        recipe_yield        TEXT,
+                        recipe_servings     INT,
+                        aggregated_rating   NUMERIC(4,2),
+                        review_count        INT
 );
 
---reviews
-create table reviews(
-    review_id int primary key ,
-    recipe_id int not null references recipes(recipe_id),
-    user_id int not null references users(author_id),
-    rating int not null,-- 数据类型？
-    review text not null,
-    date_submitted date, --导入的时候注意
-    date_modified date
+-- recipe_favorite 关系表 user favorite recipe
+CREATE TABLE recipe_favorite (
+                                 user_id   INT NOT NULL REFERENCES users(author_id),
+                                 recipe_id INT NOT NULL REFERENCES recipes(recipe_id),
+                                 PRIMARY KEY (user_id, recipe_id)
 );
 
--- likes 不需要实体表吗？
---关系表
-create table likes_relationship (
-    author_id  int not null references users(author_id),
-    review_id  int not null references reviews(review_id),
-    primary key (author_id, review_id)
+-- reviews
+CREATE TABLE reviews(
+                        review_id       INT PRIMARY KEY,
+                        recipe_id       INT NOT NULL REFERENCES recipes(recipe_id),
+                        user_id         INT NOT NULL REFERENCES users(author_id),
+                        rating          INT NOT NULL,
+                        review          TEXT NOT NULL,
+                        date_submitted  DATE,
+                        date_modified   DATE
 );
 
---keyword 实体表
-create table keyword(
-    keyword_id int primary key ,
-    keyword text not null unique  -- 防止重复！
-);
--- 关系表
-create table recipe_keyword(
-    recipe_id int not null references recipes(recipe_id),
-    keyword_id int not null references keyword(keyword_id),
-    primary key (recipe_id,keyword_id)
+-- likes 关系表
+CREATE TABLE likes_relationship (
+                                    user_id  INT NOT NULL REFERENCES users(author_id),
+                                    review_id  INT NOT NULL REFERENCES reviews(review_id),
+                                    PRIMARY KEY (user_id, review_id)
 );
 
--- ingredient 实体表
-create table ingredient(
-    ingredient_id int primary key ,
-    ingredient text not null unique --fangzhi chongfu
-);
--- 关系表
-create table recipe_ingredient(
-    recipe_id int not null references recipes(recipe_id),
-    ingredient_id int not null references ingredient(ingredient_id),
-    primary key (recipe_id,ingredient_id)
+-- keyword & 关系
+CREATE TABLE keyword(
+                        keyword_id INT PRIMARY KEY,
+                        keyword    TEXT NOT NULL UNIQUE
 );
 
---nutrition
-
-create table nutrition(
-    recipe_id int primary key references recipes(recipe_id),
-    calories numeric(7,1) not null,-- 999999.9 希望不要超范围
-    fat numeric(7,1),
-    saturated_fat numeric(7,1),
-    cholesterol numeric(7,1),
-    protein numeric(7,1),
-    sugar numeric(7,1),
-    fiber numeric(7,1),
-    carbohydrate numeric(7,1),
-    sodium numeric(7,1)
+CREATE TABLE recipe_keyword(
+                               recipe_id  INT NOT NULL REFERENCES recipes(recipe_id),
+                               keyword_id INT NOT NULL REFERENCES keyword(keyword_id),
+                               PRIMARY KEY (recipe_id, keyword_id)
 );
 
+-- ingredient & 关系
+CREATE TABLE ingredient(
+                           ingredient_id INT PRIMARY KEY,
+                           ingredient    TEXT NOT NULL UNIQUE
+);
 
---time
-create table recipe_time(
-    recipe_id int primary key references recipes(recipe_id), -- 1对1主外键
-    prepare_time interval, --这两个类型应该还要改，思考一下导入数据的时候怎么转化？？
-    cook_time interval,
-    date_published date
+CREATE TABLE recipe_ingredient(
+                                  recipe_id     INT NOT NULL REFERENCES recipes(recipe_id),
+                                  ingredient_id INT NOT NULL REFERENCES ingredient(ingredient_id),
+                                  PRIMARY KEY (recipe_id, ingredient_id)
+);
+
+-- nutrition
+CREATE TABLE nutrition(
+                          recipe_id        INT PRIMARY KEY REFERENCES recipes(recipe_id),
+                          calories         NUMERIC(7,1) NOT NULL,
+                          fat              NUMERIC(7,1),
+                          saturated_fat    NUMERIC(7,1),
+                          cholesterol      NUMERIC(7,1),
+                          protein          NUMERIC(7,1),
+                          sugar            NUMERIC(7,1),
+                          fiber            NUMERIC(7,1),
+                          carbohydrate     NUMERIC(7,1),
+                          sodium           NUMERIC(7,1)
+);
+
+-- recipe_time
+CREATE TABLE recipe_time(
+                            recipe_id       INT PRIMARY KEY REFERENCES recipes(recipe_id),
+                            prepare_time    TIME,
+                            cook_time       TIME,
+                            total_time      TIME,
+                            date_published  DATE
+);
+
+-- recipe_instruction 关系表
+CREATE TABLE recipe_instruction (
+                                    recipe_id      INT NOT NULL REFERENCES recipes(recipe_id),
+                                    instruction    TEXT NOT NULL,
+                                    primary key (recipe_id,instruction)
 );
 
 
